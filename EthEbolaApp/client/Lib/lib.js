6d9ff2e654eae1812176dda2e737e85370bd7373
ThatOtherZach <txtcla55@gmail.com>
2017-10-21 13:44:26 -0400

    Time for an app!

---
+++ EthEbolaApp/client/Lib/lib.js
@@ -0,0 +1,24 @@
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+/* Refrence for future date
+
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+
+    // Use the browser's ethereum provider
+    var provider = web3.currentProvider
+
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+  }
+
+})
+
+*/
